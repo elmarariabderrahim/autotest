996e05a80936d13dd9d8f19d7f2869fddb4a8fcb
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-02 16:57:37 +0100

    Add files via upload

---
+++ runshsc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+input="/home/soprano/Desktop/Tests/shell/TEST1/texx.txt"
+varrr=""
+while IFS= read -r line
+do
+
+varrr="${varrr}$line"
+
+
+done < "$input"
+echo "$varrr"
+
+mysql -P 32768 --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "$varrr"

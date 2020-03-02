af98d8893cdcc5cca83a2414554861fc799544af
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-02 17:36:32 +0100

    Update runshsc.sh

--- runshsc.sh
+++ runshsc.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-echo "helloooooooooooooooooooooooooooooooooooooooooooo"
+input="https://github.com/elmarariabderrahim/autotest/blob/master/texx.txt"
+varrr=""
+while IFS= read -r line
+do
+
+varrr="${varrr}$line"
+
+
+done < "$input"
+echo "$varrr"
+
+mysql -P 32768 --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "$varrr"
+

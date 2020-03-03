d3fb06926833c057b454b96911b4ca4426b189cf
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-03 14:10:02 +0100

    Update texx.sql

--- texx.sql
+++ texx.sql
@@ -1,4 +1,4 @@
 show databases;
 use db2;
-insert into Persons values (17, 'test 11', 'test 11', 'test 11', 'test 11');
+insert into Persons values (19, 'test 11', 'test 11', 'test 11', 'test 11');
 select * from Person;

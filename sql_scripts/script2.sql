81c54aec30227d1b23216f42ea003e37d81cc530
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 09:17:10 +0100

    Update script2.sql

--- sql_scripts/script2.sql
+++ sql_scripts/script2.sql
@@ -1,6 +1,3 @@
-show databases
 use db5
-insert into Persons values (587, '222222', '222', 'test 11', 'test 11')
-select * from Persons
-insert into cars values (584, 587, '22', 'test 11', 'test 11')
-select * from cars
+insert into Persons values (2, 'test2', 'test2', 'test2', 'test2');
+insert into cars values (2, 2, 'test2', 'test2', 'test2');

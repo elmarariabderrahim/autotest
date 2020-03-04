bdf24afcdb07b4b46e7e29038c26f3a1e83191f2
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 11:44:06 +0100

    Delete texx.sql

--- texx.sql
+++
@@ -1,6 +0,0 @@
-show databases;
-use db2;
-insert into Persons values (25, 'test 11', 'test 11', 'test 11', 'test 11');
-select * from Persons;
-insert into cars values (7, 11, 'test 11', 'test 11', 'test 11');
-select * from cars;

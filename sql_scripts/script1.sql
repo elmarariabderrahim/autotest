d7c1044648f58e54fb25c40c286386007ef31bf0
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 09:17:34 +0100

    Update script1.sql

--- sql_scripts/script1.sql
+++ sql_scripts/script1.sql
@@ -1,6 +1,4 @@
 show databases;
 use db5;
 insert into Persons values (1, 'test1', 'test1', 'test1', 'test1');
-select * from Persons;
 insert into cars values (1, 1, 'test1', 'test1', 'test1');
-select * from cars;

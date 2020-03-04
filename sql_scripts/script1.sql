b20c08eaee8e67ff014386516022c6cc8634b060
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 15:11:44 +0100

    Update script1.sql

--- sql_scripts/script1.sql
+++ sql_scripts/script1.sql
@@ -1,3 +1,3 @@
 use db5;
-insert into Persons values (1, 'test1', 'test1', 'test1', 'test1');
-insert into cars values (1, 1, 'test1', 'test1', 'test1');
+insert into Persons values (2, 'test1', 'test1', 'test1', 'test1');
+insert into cars values (2, 2, 'test1', 'test1', 'test1');

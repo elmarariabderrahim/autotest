e129a2fe3869ae21482e32ba24ac214200193f9f
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 11:11:43 +0100

    Update used_docker_port.sh

--- used_docker_port.sh
+++ used_docker_port.sh
@@ -3,4 +3,6 @@ str=$(docker port test-mysql)
 IFS=':'
 read -ra ADDR <<< "$str"
 docker_mysql_port=${ADDR[1]}
-
+mysql -P $docker_mysql_port --protocol=tcp -u root -pHGKO$.xu1234 -Bse "drop database if exists db5;create database db5;"
+docker exec -i 42178149b9d4 mysql -uroot -pHGKO$.xu1234 db5 < ./db1clone.sql
+mysql -P $docker_mysql_port --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "use db5; select * from Persons;"

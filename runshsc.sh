b6b001bdfaeaf0db6994d6d6f0f2e529b049cf84
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 10:24:25 +0100

    Update runshsc.sh

--- runshsc.sh
+++ runshsc.sh
@@ -10,5 +10,5 @@ varrr="${varrr}$line"
 done < "$input"
 echo "$varrr"
 
-mysql -P 32768 --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "$varrr"
+mysql -P 32769 --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "$varrr"
 done

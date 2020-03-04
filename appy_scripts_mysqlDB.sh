3c6e61e80e7dbb08a05d72c6a49cbd3d6e52674f
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-04 11:55:58 +0100

    Update appy_scripts_mysqlDB.sh

--- appy_scripts_mysqlDB.sh
+++ appy_scripts_mysqlDB.sh
@@ -1,1 +1,11 @@
-mysql -u root -pHGKO$.xu1234
+#!/bin/bash
+for f in sql_scripts/*; do
+input="./$f"
+varrr=""
+while IFS= read -r line
+do
+varrr="${varrr}$line"
+done < "$input"
+echo "$varrr"
+mysql -u root -pHGKO$.xu1234 -Bse "$varrr"
+done
